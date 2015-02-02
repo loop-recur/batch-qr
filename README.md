d567d3f77942ed04aece329088d76a05a6a76de8
Joe Nelson <cred+github@begriffs.com>
2015-02-02 11:23:09 -0800

    Read in names.csv

---
+++ README.md
@@ -0,0 +1,14 @@
+### To use
+
+Clone the repo, create a file called `names.csv` in the form
+
+```csv
+last,first,email
+```
+
+Then install deps and make the codes.
+
+```bash
+npm install
+node main.js
+```
--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 var vCard = require('vcards-js'),
-  csv = require('csv');
+  parse = require('csv-parse'),
+  fs = require('fs'),
+  v = require('vcards-js'),
+  qr = require('qrpng');
+
+fs.readFile('names.csv', 'utf8', function (err,data) {
+  parse(data, {}, function (err, people) {
+    people.map(function (p) {
+      var card = v();
+      card.firstName = p[0];
+      card.lastName = p[1];
+      card.email = p[2];
+
+      console.log(p);
+
+      qr(card.getFormattedString(), function (err, png) {
+        var pic = fs.createWriteStream(card.lastName + '-' + card.firstName + ".png");
+        pic.write(png);
+        pic.end();
+      });
+    });
+  });
+});
